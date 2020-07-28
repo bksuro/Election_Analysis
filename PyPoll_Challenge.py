006c9e94cac3d6ad45bc33eab073f05e2cec694c
bksuro <bksuro89@gmail.com>
2020-07-27 17:59:35 -0700

    PyPoll challenge deliverable files

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -6,7 +6,7 @@ import csv
 import os
 
 # Add a variable to load a file from a path.
-file_to_load = os.path.join("../Resources/election_results.csv")
+file_to_load = os.path.join("./Resources/election_results.csv")
 # Add a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
@@ -18,8 +18,8 @@ candidate_options = []
 candidate_votes = {}
 
 # 1: Create a county list and county votes dictionary.
-
-
+county_list = []
+county_votes = {}
 
 # Track the winning candidate, vote count and percentage
 winning_candidate = ""
@@ -27,7 +27,8 @@ winning_count = 0
 winning_percentage = 0
 
 # 2: Track the largest county and county voter turnout.
-
+county_with_largest_turnout = ""
+largest_turnout_votes = 0
 
 
 # Read the csv and convert it into a list of dictionaries
@@ -47,7 +48,7 @@ with open(file_to_load) as election_data:
         candidate_name = row[2]
 
         # 3: Extract the county name from each row.
-
+        county_name = row[1]
 
         # If the candidate does not match any existing candidate add it to
         # the candidate list
@@ -64,16 +65,20 @@ with open(file_to_load) as election_data:
 
         # 4a: Write a decision statement that checks that the
         # county does not match any existing county in the county list.
-
+        if county_name not in county_list:
 
             # 4b: Add the existing county to the list of counties.
-
+            county_list.append(county_name)
 
             # 4c: Begin tracking the county's vote count.
+            county_votes[county_name] = 0
 
 
         # 5: Add a vote to that county's vote count.
+        county_votes[county_name] += 1
 
+    # end for each row from csv file
+# end with reading csv data
 
 
 # Save the results to our text file.
@@ -91,24 +96,37 @@ with open(file_to_save, "w") as txt_file:
     txt_file.write(election_results)
 
     # 6a: Write a repetition statement to get the county from the county dictionary.
+    for county_name in county_list:
 
         # 6b: Initialize a variable to hold the county’s votes as they are retrieved from the county votes dictionary.
+        votes = county_votes.get(county_name)
 
         # 6c: Calculate the percent of total votes for the county.
+        vote_percentage = float(votes) / float(total_votes) * 100
+        county_results = (
+            f"{county_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
+        # 6d: Print the county results to the terminal.
+        print(county_results, end="")
 
-         # 6d: Print the county results to the terminal.
-
-         # 6e: Save the county votes to a text file.
-
-         # 6f: Write a decision statement to determine the winning county and get its vote count.
+        # 6e: Save the county votes to a text file.
+        txt_file.write(county_results)
+        # 6f: Write a decision statement to determine the winning county and get its vote count.
+        if votes > largest_turnout_votes:
+             largest_turnout_votes = votes
+             county_with_largest_turnout = county_name
 
 
     # 7: Print the county with the largest turnout to the terminal.
-
+    largest_county_turnout = (
+        f"\n-------------------------\n"
+        f"Largest County Turnout: {county_with_largest_turnout}\n"
+        f"-------------------------\n")
+    
+    print(largest_county_turnout)
 
     # 8: Save the county with the largest turnout to a text file.
-
+    txt_file.write(largest_county_turnout)
 
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Project Overview
 A Colorado Board of Elections employee has given you the following tasks to complete the election Audit of a recent local congressional election.
 
-1. Caclulate the total number of votes cast.
+1. Calculate the total number of votes cast.
 2. Get a complete list of candidates who received votes.
 3. Calculate the total number of votes each candidate received.
 4. Calculate the percentage of votes each candidate won.
@@ -28,5 +28,10 @@ The analysis of the elction show that:
     - Candidate (Diana DeGette), who received "73.8%" of the vote and "272,892" number of votes.
 
 ## Challenge Overview
+The election commission has requested some additional data to complete the audit:
+
+1. The voter turnout for each county
+2. The percentage of votes from each county out of the total count
+3. The county with the highest turnout
 
 ## Challenge Summary
